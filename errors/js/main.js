d9e438d722014733b9a35485356df43e85b1a396
Пользователь <webzavhoz@gmail.com>
2022-11-04 10:50:07 +0200

    modified errors

--- errors/js/main.js
+++ errors/js/main.js
@@ -10,6 +10,7 @@ function sum(a, b) {
 // ---------------------------Handler  Errors ( Обработчик и вывод)
 try {
     sum();                     // вызов
+    console.log('!!!!!'); // ПРИ ОШИБКЕ НЕ ОТОБРАЗИТЬСЯ_ НЕ ВЫПОЛНИТЬСЯ!!! код остановиться!
 } catch (err) {
     console.error(err.message) //вывод
 }
