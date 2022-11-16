d610d17c179b639f38b32b0b15fb89521ab7fd8e
Пользователь <webzavhoz@gmail.com>
2022-11-16 19:45:39 +0200

    validstion

---
+++ DOM/4_validation/js/app.js
@@ -0,0 +1,45 @@
+const registeredUsers = [
+    ['admin', 'KoI18'],
+    ['manager', 'SuperMe108'],
+    ['editor', '12345'],
+];
+
+
+const fields = document.getElementsByTagName('input');
+const form = document.getElementsByTagName('form')[0];
+const btn = document.getElementsByTagName('button')[0];
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    if (validate()) {
+        console.log("Access granted");      //   Доступ разрешен
+        form.reset();
+    }
+}
+
+function validate() {
+    let isValid = true;
+
+    for (let field of fields) {
+        if (!field.value.trim()) {
+            console.log("All fields are required");  // все поля обязательны к заполнению
+            isValid = false;
+        }
+    }
+
+    if (isValid) {
+        const login = fields[0].value;
+
+        const user = registeredUsers.find(user => user[0] === login);
+
+        if (!user || user[1] !== fields[1].value) {
+            console.log("Incorrect login or password");  //неверный логин или пароль
+            isValid = false;
+        }
+    }
+
+    return isValid;
+}
