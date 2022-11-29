2a9d2351af143fd219d532f2e43cea47805b5a6f
Пользователь <webzavhoz@gmail.com>
2022-11-29 15:50:36 +0200

    pacckage modules

--- __9_package/index.js
+++ __9_package/index.js
@@ -1,7 +1,11 @@
 import { addOne, sum } from './modules/first';
 import { firstName } from './modules/second';
 
-console.log(addOne(3));
+import print from './modules/third';
+
+console.log(addOne(43));
 console.log(sum(2, 2));
-console.log(firstName);
+
 console.log(`Hello from entry point. ${firstName}`);
+
+print();
---
+++ __9_package/modules/third.js
@@ -0,0 +1,7 @@
+import _ from 'lodash';
+
+export const ap = 123;
+
+export default function print() {  // default - по умолчанию только одна сущность
+    console.log('Print 3');
+}
